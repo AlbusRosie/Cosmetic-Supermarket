0b36bba675742654aec3c151af817a3526b17b2a
Ngô Thụy Thanh Tâm <ntthanhtam.forwork@gmail.com>
2025-03-20 16:17:49 +0700

    All Screens for admin role

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:ct312h_project/components/colors.dart';
+import 'package:ct312h_project/ui/admin/order/order_manager.dart';
+import 'package:ct312h_project/ui/admin/user/user_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'ui/admin/auth/auth_manager.dart';
+import 'ui/admin/order/order_screen.dart';
 import 'ui/admin/products/edit_product.dart';
 import 'ui/admin/products/products_manager.dart';
 import 'ui/admin/products/products_screen.dart';
 import 'ui/admin/products/add_product.dart';
 import 'ui/admin/auth/auth_screen.dart';
+import 'ui/admin/user/user_screen.dart';
 import 'ui/home.dart';
 import 'ui/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  // Load environment variables
-  await dotenv.load(fileName: ".env");
+  try {
+    // Load environment variables
+    await dotenv.load(fileName: ".env");
+    print("✅ Dotenv loaded successfully!");
+  } catch (e) {
+    print("❌ Dotenv loaded failed!");
+  }
+  
 
   runApp(const MyApp());
 }
@@ -29,6 +38,8 @@ class MyApp extends StatelessWidget {
       providers: [
         ChangeNotifierProvider(create: (context) => AuthManager()),
         ChangeNotifierProvider(create: (context) => ProductsManager()),
+        ChangeNotifierProvider(create: (context) => OrdersManager()),
+        ChangeNotifierProvider(create: (context) => UserManager()),
       ],
       child: Consumer<AuthManager>(
         builder: (ctx, authManager, child) {
@@ -43,7 +54,7 @@ class MyApp extends StatelessWidget {
               useMaterial3: true,
             ),
             home: authManager.isInitialized
-                ? (authManager.isAuth
+                ? (authManager.isAuth 
                     ? (authManager.isStaff
                         ? const ProductsScreen()
                         : const HomeScreen())
@@ -53,7 +64,9 @@ class MyApp extends StatelessWidget {
               ProductsScreen.routeName: (context) => const ProductsScreen(),
               AddProductScreen.routeName: (context) => const AddProductScreen(),
               EditProductScreen.routeName: (ctx) => EditProductScreen(),
+              OrdersScreen.routeName: (ctx) => OrdersScreen(),
               HomeScreen.routeName: (context) => const HomeScreen(),
+              UsersScreen.routeName: (context) => const UsersScreen(),
             },
             onUnknownRoute: (settings) {
               return MaterialPageRoute(
